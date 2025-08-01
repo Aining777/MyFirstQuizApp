0732a86c5b7aaa2f3ef45a2c2c64e4d6f50ef3bb
Aining777 <55470640+Aining777@users.noreply.github.com>
2025-08-01 19:32:20 +0800

    Add files via upload

---
+++ java/com/example/dailyquizapp/ui/Color.kt
@@ -0,0 +1,13 @@
+package com.example.dailyquizapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 定义Compose Material Design 3 默认使用的颜色
+// 您可以根据自己的品牌或喜好修改这些颜色
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
